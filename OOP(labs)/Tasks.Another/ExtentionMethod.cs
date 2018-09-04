02bbddb610d7364439a4a01382ca8a98bd60ac5c
Aleksandra <oleksandra.kryvko@nure.ua>
2018-09-04 20:37:43 +0300

    Add new extention method - Quicksort

---
+++ OOP(labs)/Tasks.Another/ExtentionMethod.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Создать расширяющий метод для целочисленного массива, который сортирует элементы массива по возрастанию
+namespace Tasks.Another
+{
+    static class ExtentionMethod
+    {
+        public static void QuickSortArr(this int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                if (pivot > 1)
+                {
+                    QuickSortArr(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    QuickSortArr(arr, pivot + 1, right);
+                }
+            }
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right]) return right;
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+
+
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
+}
--- OOP(labs)/Tasks.Another/Program.cs
+++ OOP(labs)/Tasks.Another/Program.cs
@@ -10,24 +10,46 @@ namespace Tasks.Another
     {
         static void Main(string[] args)
         {
-            Store store = new Store();
+            //Store store = new Store();
 
-            Console.WriteLine("Input name of product");
+            //Console.WriteLine("Input name of product");
 
-            string userInput = Console.ReadLine();
+            //string userInput = Console.ReadLine();
 
-            bool result = store.IsSearchInformation(userInput);
-            
-            if(result)
+            //bool result = store.IsSearchInformation(userInput);
+
+            //if(result)
+            //{
+            //   string outputInfo = store.WriteInfo(userInput);
+            //    Console.WriteLine(outputInfo);
+            //    Console.WriteLine(store[128975]);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Such product is absent in our shop");
+            //}
+
+            int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+
+            Console.WriteLine("Original array : ");
+            foreach (var item in arr)
             {
-               string outputInfo = store.WriteInfo(userInput);
-                Console.WriteLine(outputInfo);
-                Console.WriteLine(store[128975]);
+                Console.Write(" " + item);
             }
-            else
+            Console.WriteLine();
+
+            arr.QuickSortArr(0, arr.Length - 1);
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted array : ");
+
+            foreach (var item in arr)
             {
-                Console.WriteLine("Such product is absent in our shop");
+                Console.Write(" " + item);
             }
+            Console.WriteLine();
+
+
             Console.ReadKey();
         }
     }
