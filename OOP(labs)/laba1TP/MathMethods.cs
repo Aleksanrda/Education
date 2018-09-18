b2e041570e70f4bd96d3e6ed0db058f0798aacc6
Aleksandra <oleksandra.kryvko@nure.ua>
2018-09-18 19:58:30 +0300

    Add new method AverageVariance

---
+++ OOP(labs)/laba1TP/MathMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1TP
+{
+    public class MathMethods
+    {
+        public int AverageVariance(int[] arrayOfNumbers)
+        {
+            int sum = 0;
+            int averageVariance = 0;
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                sum += arrayOfNumbers[i];
+            }
+
+            averageVariance = sum / arrayOfNumbers.Length;
+            return averageVariance;
+        }
+
+
+    }
+}
