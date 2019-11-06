01348ffbf51874fe0b1e4a265f6f3eb18a06f4ac
Alexandra <oleksandra.kryvko@nure.ua>
2019-11-06 23:50:39 +0200

    add attributes in class Subscription

--- Creeperscare/Creeperscare.Entities/Subscription.cs
+++ Creeperscare/Creeperscare.Entities/Subscription.cs
@@ -8,5 +8,17 @@ namespace Creeperscare.Entities
 {
     public class Subscription
     {
+        public int SubscriptionId { get; set; }
+
+        public string IdentificationName { get; set; }
+
+        public double Price { get; set; }
+
+        public List<User> Subscribers { get; set; }
+
+        public Subscription()
+        {
+            this.Subscribers = new List<User>();
+        }
     }
 }
