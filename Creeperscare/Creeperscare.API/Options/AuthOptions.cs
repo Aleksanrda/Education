e1b3b8fbdfa563f5a4fef35e69bbd3db462de962
Alexandra <oleksandra.kryvko@nure.ua>
2019-11-13 22:34:04 +0200

    Fill AuthOptions class

--- Creeperscare/Creeperscare.API/Options/AuthOptions.cs
+++ Creeperscare/Creeperscare.API/Options/AuthOptions.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Creeperscare.API.Options
 {
     public class AuthOptions
     {
+        public const string Issuer = "TokenIssuer";
+        public const string Audience = "http://localhost:5000/";
+        public const string Key = "fdkjfhkerjwoewkddkvbdfkw";
+        public const int Lifetime = 60;
+
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Key));
+        }
     }
 }
