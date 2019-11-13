a6fbf9f6dafe44895ca67d7483472c961d0c6757
Alexandra <oleksandra.kryvko@nure.ua>
2019-11-13 22:53:49 +0200

    Add GardenPlotModel

---
+++ Creeperscare/Creeperscare.API/Models/GardenPlotModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creeperscare.API.Models
+{
+    public class GardenPlotModel
+    {
+        public int GardenPlotId { get; set; }
+
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+
+    }
+}
