4fb8f0d618108d346a7ad7ad0462de8551346867
Alexandra <oleksandra.kryvko@nure.ua>
2019-11-13 22:25:46 +0200

    Update class User

--- Creeperscare/Creeperscare.API/Entities/User.cs
+++ Creeperscare/Creeperscare.API/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,17 +8,9 @@ using System.Threading.Tasks;
 
 namespace Creeperscare.Entities
 {
-    public class User
+    public class User : IdentityUser
     {
-        public int UserId { get; set; }
-
-        public string Name { get; set; }
-
         public string Role { get; set; }
-
-        public string Email { get; set; }
-
-        public string Password { get; set; }
 
         public int? SubscriptionId { get; set; }
 
