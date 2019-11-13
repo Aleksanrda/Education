26b1526e6ff1a823fca05d38af66ac59bb6f60dc
Alexandra <oleksandra.kryvko@nure.ua>
2019-11-13 22:42:05 +0200

    Create DeviceModel

---
+++ Creeperscare/Creeperscare.API/Models/DeviceModel.cs
@@ -0,0 +1,21 @@
+﻿using Creeperscare.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creeperscare.API.Models
+{
+    public class DeviceModel
+    {
+        public int DeviceId { get; set; }
+
+        public double ActionRange { get; set; }
+
+        public double Humidity { get; set; }
+
+        public double Temperature { get; set; }
+
+        public User Owner { get; set; }
+    }
+}
