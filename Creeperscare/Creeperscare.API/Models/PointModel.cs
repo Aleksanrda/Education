8edc6b04c7a18e15dd0bc9bbc18de4b2672f90dd
Alexandra <oleksandra.kryvko@nure.ua>
2019-11-15 19:02:52 +0200

    Create PointModel

---
+++ Creeperscare/Creeperscare.API/Models/PointModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creeperscare.API.Models
+{
+    public class PointModel
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
