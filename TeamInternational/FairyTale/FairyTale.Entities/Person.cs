c4963d3da7157032cf96369342bc32efc54bab96
Alexandra <oleksandra.kryvko@nure.ua>
2020-03-01 19:22:11 +0200

    Add Person class

---
+++ TeamInternational/FairyTale/FairyTale.Entities/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FairyTale.Entities
+{
+    public class Person : Character
+    {
+        public string Position { get; set; }
+
+        public int Salary { get; set; }
+    }
+}
