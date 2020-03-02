a1cea7b1384509ebc9e58b16705a5badcfd52829
Alexandra <oleksandra.kryvko@nure.ua>
2020-03-02 14:42:30 +0200

    Add characters in fairy tale

--- TeamInternational/FairyTale/FairyTale.Turnip/Program.cs
+++ TeamInternational/FairyTale/FairyTale.Turnip/Program.cs
@@ -1,4 +1,5 @@
-﻿using FairyTale.Turnip.Services;
+﻿using FairyTale.Entities;
+using FairyTale.Turnip.Services;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -22,10 +23,72 @@ namespace FairyTale.Turnip
             var turnipManager = new TurnipManager(characterService);
 
             turnipManager.onTurnipIsPulled += characterService.PullTurnip;
-                
+
             turnipManager.onTurnipIsNotPulled += characterService.DragTurnip;
 
             turnipManager.Run();
+
+            Console.WriteLine("Demonstration of the collection");
+
+            Character grandPa = new Person
+            {
+                Name = "Ivan",
+                Age = 95,
+                CountLikes = 10000,
+                Personality = "grandPa",
+                Position = "Plants turnip",
+                Salary = 100
+            };
+
+            Character grandMa = new Person
+            {
+                Name = "Anna",
+                Age = 94,
+                CountLikes = 500,
+                Personality = "grandma",
+                Position = "Love grandPa",
+                Salary = 1000
+            };
+
+            Character grandDaughter = new Person
+            {
+                Name = "Masha",
+                Age = 15,
+                CountLikes = 10000,
+                Personality = "grandDaughter",
+                Position = "Very beautiful and energic",
+                Salary = 2000
+            };
+
+            Character dog = new Animal()
+            {
+                Name = "Sharic",
+                Age = 5,
+                CountLikes = 100,
+                Personality = "dog",
+                Type = AnimalType.Dog,
+                Strength = 50
+            };
+
+            Character cat = new Animal()
+            {
+                Name = "Sneshka",
+                Age = 3,
+                CountLikes = 150,
+                Personality = "cat",
+                Type = AnimalType.Cat,
+                Strength = 100
+            };
+
+            Character mouse = new Animal()
+            {
+                Name = "Myri",
+                Age = 1,
+                CountLikes = 200,
+                Personality = "mouse",
+                Type = AnimalType.Mouse,
+                Strength = 200000
+            };
 
             Console.ReadKey();
         }
