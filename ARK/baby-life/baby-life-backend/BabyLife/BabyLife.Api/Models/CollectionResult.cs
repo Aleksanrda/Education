1838c3cc836599cf8055fe3916831ea6b3c50e9c
Alexandra <oleksandra.kryvko@nure.ua>
2020-03-23 20:58:46 +0200

    Add CollectionResult class

---
+++ ARK/baby-life/baby-life-backend/BabyLife/BabyLife.Api/Models/CollectionResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BabyLife.Api.Models
+{
+    public class CollectionResult<T>
+    {
+        public IEnumerable<T> Items { get; }
+
+        public int TotalAmount { get; }
+
+        public CollectionResult(IEnumerable<T> items, int totalAmount)
+        {
+            Items = items;
+            TotalAmount = totalAmount;
+        }
+    }
+}
