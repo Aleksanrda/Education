41a49ded378b4163b6e1e57097288eb1a3a7e0a7
Alexandra <oleksandra.kryvko@nure.ua>
2020-03-31 19:44:04 +0300

    Create ProductList.Core

---
+++ TeamInternational/ProductList/ProductList.Core/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProductList.Core
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public int Count { get; set; }
+
+        public Value Value { get; set; }
+    }
+}
---
+++ TeamInternational/ProductList/ProductList.Core/Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductList.Core
+{
+    public enum Value
+    {
+        Pieces,
+        Liter,
+        Gram
+    }
+}
