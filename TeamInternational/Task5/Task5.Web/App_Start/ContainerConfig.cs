793bcc21f4688d2274986ccaf2f8b51df48310de
Alexandra <oleksandra.kryvko@nure.ua>
2020-04-16 20:17:24 +0300

    Add get flowers

--- TeamInternational/Task5/Task5.Web/App_Start/ContainerConfig.cs
+++ TeamInternational/Task5/Task5.Web/App_Start/ContainerConfig.cs
@@ -3,12 +3,15 @@ using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using Task5.Api.Services;
 using Task5.Core.Entities;
 using Task5.Core.Repositories;
+using Task5.DataAccess;
 using Task5.DataAccess.Repositories;
 
 namespace Task5.Web.App_Start
@@ -22,11 +25,13 @@ namespace Task5.Web.App_Start
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
             builder.RegisterApiControllers(typeof(MvcApplication).Assembly);
 
-            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().SingleInstance();
+            builder.RegisterType<Task5DbContext>().AsSelf();
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
 
             builder.RegisterType(typeof(Repository<Flower>))
                 .As(typeof(IRepository<Flower>))
-                .SingleInstance();
+                .InstancePerRequest();
 
             builder.RegisterType(typeof(Repository<Plantation>))
                 .As(typeof(IRepository<Plantation>))
@@ -51,6 +56,8 @@ namespace Task5.Web.App_Start
             builder.RegisterType(typeof(Repository<SupplyFlower>))
                 .As(typeof(IRepository<SupplyFlower>))
                 .SingleInstance();
+
+            builder.RegisterType<FlowersService>().As<IFlowersService>().InstancePerRequest();
 
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
---
+++ TeamInternational/Task5/Task5.Web/Controllers/FlowersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task5.Api.Services;
+
+namespace Task5.Web.Controllers
+{
+    public class FlowersController : Controller
+    {
+        private readonly IFlowersService flowersService;
+
+        public FlowersController(IFlowersService flowersService)
+        {
+            this.flowersService = flowersService;
+        }
+
+        public ActionResult Index()
+        {
+            var model = flowersService.GetFlowers();
+            return View(model);
+        }
+    }
+}
